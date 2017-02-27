bf12005c519f3917ce52b14e00a865172af853c9
kiwan.ryu <ryukw7@gmail.com>
2017-02-27 21:43:32 +0900

    A unit test for NaHttp class.

---
+++ NaLib/NaLib_UnitTest/test_class_NaHttp.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include <catch/catch.hpp>
+
+#include "../NaHttp.h"
+
+TEST_CASE("NaHttp.SendRequest")
+{
+	NaHttp http;
+	NaString url(L"www.daum.net");
+	http.Open(url);
+	http.SendRequest();
+	NaString resp = http.GetResponse();
+	CHECK(resp.GetLength() > 0);
+}

9f1378b21ad525e9150ece910f71731022224ecf
neoarc <neoarcturus@gmail.com>
2017-07-05 12:49:36 +0900

    Added test code for NaCurl.Get

--- test/test_class_NaCurl.cpp
+++ test/test_class_NaCurl.cpp
@@ -12,3 +12,21 @@ TEST_CASE("NaCurl.Post", "[.]")
 	auto strLeft = strRet.Left(strBeginningOfHtml.GetLength());
 	CHECK(strLeft == L"<!doctype html>");
 }
+
+TEST_CASE("NaCurl.Get", "[.]")
+{
+	NaCurl curl;
+
+	char *outBuf = nullptr;
+	long lSize = -1;
+	bool bRet = curl.Get(
+		L"[Your-Server-Url]/update/Resources/Sprite/Font.bmp",
+		outBuf, lSize
+	);
+	
+	CHECK(bRet == true);
+
+	CHECK(outBuf != nullptr);
+
+	CHECK(lSize == 49208);
+}

d939e6da6f768756725d69443f6bb5d65dacef3e
neoarc <neoarcturus@gmail.com>
2017-08-28 15:54:40 +0900

    Updated batch: _update_nalib.bat (include process to setup git remote)

--- _update_nalib.bat
+++ _update_nalib.bat
@@ -1,2 +1,11 @@
+@echo off
+echo [Batch] Checking remote nalib...
+git remote get-url nalib > NUL | find /I /N "fatal: No such remote" > NUL
+if "%ERRORLEVEL%" == "0" (
+	echo [Batch] Adding remote nalib...
+	git remote add nalib https://github.com/neoarc/NaLib.git
+)
+echo [Batch] Pull subtree ...
 git subtree pull -P src/NaLib nalib master
+echo [Batch] Done!
 @pause
